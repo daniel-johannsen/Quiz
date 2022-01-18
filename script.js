1579188c58567550c4fd8548539fdbb76d325454
aniel Johannsen <daniel-johannsen@web.de>
2022-01-18 23:59:36 +0100

    created card  and begin JSON

---
+++ script.js
@@ -0,0 +1,29 @@
+let questions = [{
+        'question': 'Wer hat HTML erfunden?',
+        'answer': 'Robbie Williams',
+        'answer': '',
+        'answer': '',
+        'answer': ''
+    },
+    {
+        'question': 'Wer hat HTML erfunden?',
+        'answer': 'Robbie Williams',
+        'answer': '',
+        'answer': '',
+        'answer': ''
+    },
+    {
+        'question': 'Wer hat HTML erfunden?',
+        'answer': 'Robbie Williams',
+        'answer': '',
+        'answer': '',
+        'answer': ''
+    },
+    {
+        'question': 'Wer hat HTML erfunden?',
+        'answer': 'Robbie Williams',
+        'answer': '',
+        'answer': '',
+        'answer': ''
+    }
+];
